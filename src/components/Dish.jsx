3ac6f166c633804ee27b198a0d081a6d111cfc1e
koa102025-cmd <koa102025@gmail.com>
2026-01-30 12:10:42 +0100

    finished

--- src/components/Dish.jsx
+++ src/components/Dish.jsx
@@ -4,9 +4,11 @@ export default function Dish(props) {
 	return (
 		<div className={styles.dishCard}>
 			<h2>{props.tittel}</h2>
-			<p className={styles.pris}>{props.pris}</p>
+			<div className={styles.rightPris}>
+				<p className={styles.ingredienser}>{props.ingredienser}</p>
 
-			<p className={styles.ingredienser}>{props.ingredienser}</p>
+				<p className={styles.pris}>{props.pris}</p>
+			</div>
 
 			<p className={styles.kategori}>{props.kategori}</p>
 		</div>
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -3,19 +3,58 @@ import data from "../data";
 import styles from "../styles/Menu.module.css";
 
 export default function Menu() {
-	const menuElement = data.map((dish) => {
-		return <Dish key={dish.id} {...dish} />;
-	});
+	const order = ["Forrett", "Hovedrett", "Dessert"];
 
 	return (
-		<main>
-			<div className={styles.logo}>
-				{" "}
-				<img src="/menu-logo.png" alt="menu logo" />
-				<h1>VÅR MENU</h1>
-			</div>
+		<div className={styles.all}>
+			<header className={styles.header}>
+				<div className={styles.left}>
+					<p>+4798579720</p>
+					<p>www.dailygo.com</p>
+					<p>dailygo@gmail.com</p>
+					<p>OPEN DAILY 6:00 - 24.00</p>
+				</div>
+				<div className={styles.center}>
+					<img
+						src={`${import.meta.env.BASE_URL}menu-logo.png`}
+						alt="menu logo"
+						className={styles.logo}
+					/>
+					<h1>
+						<span class="daily">Daily</span>
+						<span class="go">Go</span>
+					</h1>
+				</div>
 
-			<div className={styles.menuContainer}>{menuElement}</div>
-		</main>
+				<div className={styles.right}>
+					<p>
+						Oppdag en harmonisk smaksopplevelse i hver rett. Vi bruker kun
+						friske, lokale råvarer for å gjøre ditt måltid til dagens
+						høydepunkt.
+					</p>
+				</div>
+			</header>
+			<main className={styles.main}>
+				{order.map((categoryName) => {
+					const categoryDishes = data.filter(
+						(dish) => dish.kategori === categoryName,
+					);
+
+					if (categoryDishes.length === 0) return null;
+
+					return (
+						<section key={categoryName} className={styles.categorySection}>
+							<h2 className={styles.categoryTitle}>{categoryName}</h2>
+
+							<div className={styles.menuContainer}>
+								{categoryDishes.map((dish) => (
+									<Dish key={dish.id} {...dish} />
+								))}
+							</div>
+						</section>
+					);
+				})}
+			</main>
+		</div>
 	);
 }
